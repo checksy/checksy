efcd46536987b58dcb8d3b59b25048b206a995e6
Nick Scialli <538045+nas5w@users.noreply.github.com>
2019-01-26 16:28:07 -0500

    Add type tests

--- example.js
+++ example.js
@@ -1,16 +1,13 @@
-import { addRules, isString, validate, Validator } from "../index.js";
+import { addRules, isNumber, isString, validate, Validator } from "./index.js";
 
 let validator = new Validator();
 
 addRules(validator, [
-  {
-    prop: "name",
-    test: isString,
-    message: "Name must be a string"
-  }
+  { prop: "name", test: isString, message: "Name must be a string." },
+  { prop: "age", test: isNumber, message: "Age must be a number." }
 ]);
 
-const obj = { name: 23 };
+const obj = { name: "Nick", age: 32 };
 
 let results = validate(obj, validator);
 
--- index.js
+++ index.js
@@ -6,8 +6,40 @@ function addRules(validator, rules) {
   validator.rules = validator.rules.concat(rules);
 }
 
+function isBoolean(prop) {
+  return typeof prop === "boolean";
+}
+
+function isFalse(prop) {
+  return prop === false;
+}
+
+function isFunction(prop) {
+  return typeof prop === "function";
+}
+
+function isNull(prop) {
+  return prop === null;
+}
+
+function isNumber(prop) {
+  return typeof prop === "number";
+}
+
 function isString(prop) {
   return typeof prop === "string";
+}
+
+function isSymbol(prop) {
+  return typeof prop === "symbol";
+}
+
+function isTrue(prop) {
+  return prop === true;
+}
+
+function isUndefined(prop) {
+  return typeof prop === "undefined";
 }
 
 function validate(obj, validator) {
@@ -24,7 +56,15 @@ function validate(obj, validator) {
 
 module.exports = {
   addRules,
+  isBoolean,
+  isFalse,
+  isFunction,
+  isNull,
+  isNumber,
   isString,
+  isSymbol,
+  isTrue,
+  isUndefined,
   validate,
   Validator
 };
